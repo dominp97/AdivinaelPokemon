d777a7d9dd69155954f72381d813f210f833a332
dominp97 <36171336+dominp97@users.noreply.github.com>
2019-01-16 18:30:31 +0100

    añadidas imagenes

--- app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
+++ app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
@@ -2,18 +2,12 @@ package com.pmdm.adivinaelpokemon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class JuegoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    public void lanzarJuego(View view){
-
+        setContentView(R.layout.activity_juego);
     }
 }
