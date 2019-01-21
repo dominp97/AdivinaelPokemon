46462fd2386db449b15059ef28b116e0960a4848
dominp97 <36171336+dominp97@users.noreply.github.com>
2019-01-21 21:30:49 +0100

    primer boton insertado programaticamente

--- app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
+++ app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
@@ -1,13 +1,26 @@
 package com.pmdm.adivinaelpokemon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class JuegoActivity extends AppCompatActivity {
+
+    LinearLayout lNImagenes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        lNImagenes = findViewById(R.id.imagenes);
+        Button btn = new Button(this);
+
+        lNImagenes.addView(btn);
     }
+
+
 }
