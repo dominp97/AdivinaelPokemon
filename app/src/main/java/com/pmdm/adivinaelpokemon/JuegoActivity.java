cd7d9e2fb8ea5d444ca05fe2ed857e8bdfc21e55
dominp97 <36171336+dominp97@users.noreply.github.com>
2019-01-30 19:14:39 +0100

    cambios MUY IMPORTANTES, funciona el cambio de orientacion

--- app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
+++ app/src/main/java/com/pmdm/adivinaelpokemon/JuegoActivity.java
@@ -3,6 +3,7 @@ package com.pmdm.adivinaelpokemon;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.AttributeSet;
@@ -15,6 +16,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
+
+import static android.content.res.Configuration.ORIENTATION_LANDSCAPE;
 
 public class JuegoActivity extends AppCompatActivity {
 
@@ -33,49 +36,39 @@ public class JuegoActivity extends AppCompatActivity {
         LinearLayout[] listaLineales = new LinearLayout[cantidadImagenes/3];
 
         ViewGroup.LayoutParams lP = new LinearLayout.LayoutParams(0,
-               250);
-        //ViewGroup.LayoutParams.WRAP_CONTENT
+                ViewGroup.LayoutParams.WRAP_CONTENT);
         ((LinearLayout.LayoutParams) lP).weight = 1;
 
+        ViewGroup.LayoutParams lP0 = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,250);
+        ((LinearLayout.LayoutParams) lP).weight = 1;
+
+
+        Button btn22 = findViewById(R.id.button2);
+        btn22.setText(lNImagenes.getLayoutParams().height+"");
+
+        int altura = lNImagenes.getLayoutParams().height /3;
+
+
+        //Parametros layout linear
+        ViewGroup.LayoutParams llP = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,0);//altura
+        ((LinearLayout.LayoutParams) llP).weight = 3;
+
+
+        //Creamos botones
         ImageButton btn = new ImageButton(this);
         btn.setImageDrawable(getResources().getDrawable(R.drawable.abra));
         ImageButton btn1 = new ImageButton(this);
         btn1.setImageDrawable(getResources().getDrawable(R.drawable.eevee));
         ImageButton btn2 = new ImageButton(this);
         btn2.setImageDrawable(getResources().getDrawable(R.drawable.mew));
-
-        btn.setLayoutParams(lP);
-        btn1.setLayoutParams(lP);
-        btn1.setForegroundGravity(Gravity.CENTER_HORIZONTAL);
-        btn2.setLayoutParams(lP);
-
-        listaLineales[0] = new LinearLayout(this);
-        listaLineales[0].addView(btn);
-        listaLineales[0].addView(btn1);
-
-        listaLineales[0].addView(btn2);
-        lNImagenes.addView(listaLineales[0]);
-
-
-        //Segundo linear
         ImageButton btn4 = new ImageButton(this);
         btn4.setImageDrawable(getResources().getDrawable(R.drawable.charmander));
         ImageButton btn5 = new ImageButton(this);
         btn5.setImageDrawable(getResources().getDrawable(R.drawable.bullbasaur));
         ImageButton btn6 = new ImageButton(this);
         btn6.setImageDrawable(getResources().getDrawable(R.drawable.squirtle));
-
-        btn4.setLayoutParams(lP);
-        btn5.setLayoutParams(lP);
-        btn6.setLayoutParams(lP);
-        listaLineales[1] = new LinearLayout(this);
-        listaLineales[1].addView(btn4);
-        listaLineales[1].addView(btn5);
-        listaLineales[1].addView(btn6);
-        lNImagenes.addView(listaLineales[1]);
-
-
-        //Tercer linear
         ImageButton btn8 = new ImageButton(this);
         btn8.setImageDrawable(getResources().getDrawable(R.drawable.jigglypuff));
         ImageButton btn9 = new ImageButton(this);
@@ -83,25 +76,66 @@ public class JuegoActivity extends AppCompatActivity {
         ImageButton btn10 = new ImageButton(this);
         btn10.setImageDrawable(getResources().getDrawable(R.drawable.venonat));
 
-        btn8.setLayoutParams(lP);
-        btn9.setLayoutParams(lP);
-        btn10.setLayoutParams(lP);
+
+        //Creamos layouts
+        listaLineales[0] = new LinearLayout(this);
+        listaLineales[1] = new LinearLayout(this);
         listaLineales[2] = new LinearLayout(this);
+
+
+
+        listaLineales[0].addView(btn);
+        listaLineales[0].addView(btn1);
+        listaLineales[0].addView(btn2);
+        listaLineales[1].addView(btn4);
+        listaLineales[1].addView(btn5);
+        listaLineales[1].addView(btn6);
         listaLineales[2].addView(btn8);
         listaLineales[2].addView(btn9);
         listaLineales[2].addView(btn10);
+
+
+        lNImagenes.addView(listaLineales[0]);
+        lNImagenes.addView(listaLineales[1]);
         lNImagenes.addView(listaLineales[2]);
 
-        TextView tv = findViewById(R.id.cachoTexto);
+
+        listaLineales[0].setLayoutParams(llP);
+        listaLineales[1].setLayoutParams(llP);
+        listaLineales[2].setLayoutParams(llP);
 
 
+        btn.setLayoutParams(lP);
+        btn1.setLayoutParams(lP);
+        btn2.setLayoutParams(lP);
+        btn4.setLayoutParams(lP);
+        btn5.setLayoutParams(lP);
+        btn6.setLayoutParams(lP);
+        btn8.setLayoutParams(lP);
+        btn9.setLayoutParams(lP);
+        btn10.setLayoutParams(lP);
 
 
         //Creamos random de nombres
+
+        TextView tv1 = new TextView(this);
+
         Resources res = getResources();
         String[] poke = res.getStringArray(R.array.pokemon);
         Random r = new Random();
-        tv.setText(poke[r.nextInt(10)]);
+        tv1.setText(poke[r.nextInt(10)]);
+
+        //Atributos texto
+        ViewGroup.LayoutParams llPt = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);//altura
+        ((LinearLayout.LayoutParams) llP).weight = 1;
+
+        tv1.setLayoutParams(llPt);
+        lNImagenes.addView(tv1);
+
+
+        new contador().execute("");
+
     }
 
 
@@ -110,6 +144,36 @@ public class JuegoActivity extends AppCompatActivity {
 
 
         return "";
+    }
+
+
+    private class contador extends AsyncTask<String, Void, String> {
+
+        private Button btnT;
+        private int contrareloj;
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+                while(contrareloj > 60){
+                    this.wait(1000);
+                    contrareloj--;
+                    btnT.setText(contrareloj+"");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                btnT.setText("No va");
+            }
+            return contrareloj+"";
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            contrareloj = 60;
+            btnT = findViewById(R.id.button2);
+        }
     }
 
 
